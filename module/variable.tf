11f33f2fabc608ba837d487bd913414be4d98a47
Navinder Gill <126389118+navigill7@users.noreply.github.com>
2025-04-13 08:57:14 +0000

    changes the variable configurations

--- module/variable.tf
+++ module/variable.tf
@@ -14,3 +14,42 @@ variable "vpc_name" {
 variable "env" {
   
 }
+
+variable "igw_name" {
+  
+}
+
+variable "eks_public_subnet_count" {
+  
+}
+
+variable "pub_cidr_block" {
+  
+
+}
+
+variable "private_cidr_block" {
+  
+}
+
+variable "pub_az" {
+  
+}
+
+variable "pri-az" {
+  
+}
+
+
+variable "pub_sub_name" {
+  
+}
+
+variable "pri_sub_name" {
+  
+
+}
+
+variable "eks_private_subnet_count" {
+  
+}
