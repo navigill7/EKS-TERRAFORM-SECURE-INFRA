af62c9772fd9a8a1ace3cc47988965d7f9dbcc4a
Navinder Gill <126389118+navigill7@users.noreply.github.com>
2025-04-13 12:08:33 +0000

    created iam role nodesgroup

--- module/iam.tf
+++ module/iam.tf
@@ -30,3 +30,43 @@ resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
   role       = aws_iam_role.eks-cluster-role[count.index].name
 }
+
+// IAM role for worker nodes 
+
+resource "aws_iam_role" "eks-nodegroup-role" {
+  count = var.is_eks_role_enabled? 1 : 0
+  name  = "${local.cluster-name}-nodegroup-role-${random_integer.random_suffix.result}"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+  })
+}
+
+
+resource "aws_iam_role_policy_attachment" "eks-AmazonWorkerNodePolicy" {
+  count      = var.is_eks_role_enabled ? 1 : 0
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.eks-nodegroup-role[count.index].name
+}
+
+
+resource "aws_iam_role_policy_attachment" "eks-AmazonEKS_CNI_Policy" {
+  count      = var.is_eks_role_enabled ? 1 : 0
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.eks-nodegroup-role[count.index].name
+}
+
+resource "aws_iam_role_policy_attachment" "eks-AmazonEC2ContainerRegistryReadOnly" {
+  count      = var.is_eks_role_enabled ? 1 : 0
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.eks-nodegroup-role[count.index].name
+}
+
+
