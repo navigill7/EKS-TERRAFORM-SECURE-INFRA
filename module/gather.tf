b28369b658a156252424a9b463e4c29c0acb4ac2
Navinder Gill <126389118+navigill7@users.noreply.github.com>
2025-04-14 14:32:52 +0000

    updating  oidc policy document

--- module/gather.tf
+++ module/gather.tf
@@ -11,12 +11,12 @@ data "aws_iam_policy_document" "eks-oidc-document" {
 
     condition {
       test     = "StringEquals"
-      variable = "${replace(aws_iam_openid_connect_provider.eks-oidc.url, "https://", "")}:sub"
+      variable = "${replace(aws_iam_openid_connect_provider.eks_oidc_provider.url, "https://", "")}:sub"
       values   = ["system:serviceaccount:default:aws-test"]
     }
 
     principals {
-      identifiers = [aws_iam_openid_connect_provider.eks-oidc.arn]
+      identifiers = [aws_iam_openid_connect_provider.eks_oidc_provider.arn]
       type        = "Federated"
     }
   }
