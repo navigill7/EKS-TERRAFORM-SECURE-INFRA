d9cab3ee9ef44d0cf8ff39b4cdde4eb5178aa11a
Navinder Gill <126389118+navigill7@users.noreply.github.com>
2025-04-14 14:25:38 +0000

    updated vpc configuration

--- module/vpc.tf
+++ module/vpc.tf
@@ -1,24 +1,3 @@
-locals {
-  cluster-name = var.cluster-name
-}
-
-resource "aws_vpc" "eks-vpc" {
-  cidr_block = var.cidr_block
-  instance_tenancy = "default"
-  enable_dns_hostnames = true
-  enable_dns_support = true
-
-  tags = {
-    Name = var.vpc_name
-    Env = var.env
-  }
-}
-
-
-locals {
-  cluster-name = var.cluster-name
-}
-
 resource "aws_vpc" "eks-vpc" {
   cidr_block = var.cidr_block
   instance_tenancy = "default"
