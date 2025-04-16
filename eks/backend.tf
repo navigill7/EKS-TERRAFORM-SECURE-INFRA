a682cd71c18ff206a6f7af93e2eff5b7b36a5667
Navinder Gill <126389118+navigill7@users.noreply.github.com>
2025-04-16 11:08:48 +0000

    adding kubernetes deplyment manifests

--- eks/backend.tf
+++ eks/backend.tf
@@ -28,20 +28,8 @@ provider "aws" {
   region = var.region
 }
 
-data "aws_eks_cluster" "eks" {
-  name = module.cluster_name
-}
-
-data "aws_eks_cluster_auth" "eks" {
-  name = module.cluster_name
-}
-
 provider "helm" {
   kubernetes {
-    host                   = data.aws_eks_cluster.eks.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
-    token                  = data.aws_eks_cluster_auth.eks.token
-    load_config_file       = false
+    config_path = "~/.kube/config"
   }
 }
-
