3e6d81eebac9f31d674e55c65b3b9e0ac3126cb4
Navinder Gill <126389118+navigill7@users.noreply.github.com>
2025-04-16 07:40:12 +0000

    adding helm provider configurations

--- eks/backend.tf
+++ eks/backend.tf
@@ -28,8 +28,20 @@ provider "aws" {
   region = var.region
 }
 
+data "aws_eks_cluster" "eks" {
+  name = aws_eks_cluster.eks[0].name
+}
+
+data "aws_eks_cluster_auth" "eks" {
+  name = aws_eks_cluster.eks[0].name
+}
+
 provider "helm" {
   kubernetes {
-    config_path = "~/.kube/config"
+    host                   = data.aws_eks_cluster.eks.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.eks.token
+    load_config_file       = false
   }
 }
+
