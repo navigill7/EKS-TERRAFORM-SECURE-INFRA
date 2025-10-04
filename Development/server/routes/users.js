4c6244c8ced40fec7982b157bfb0b8451ac62502
Navinder Gill <nnavigill784@gmail.com>
2025-10-04 15:13:43 +0530

    adding the routes for social media urls

--- Development/server/routes/users.js
+++ Development/server/routes/users.js
@@ -1,25 +1,27 @@
 import express from "express"
 import {
     getUser,
-    getUserFriends ,
-    addRemoveFriends ,
-    SearchUser
+    getUserFriends,
+    addRemoveFriends,
+    SearchUser,
+    updateSocialUrls  // Add this new import
 } from "../controllers/users.js";
 import { verifyToken } from "../middleware/auth.js";
 
 const router = express.Router();
 
- // READ //
-router.get("/:id" , verifyToken , getUser);
 
-router.get("/:id/friends" , verifyToken , getUserFriends);
+router.get("/:id", verifyToken, getUser);
+router.get("/:id/friends", verifyToken, getUserFriends);
 
- // UPDATE //
 
-router.patch("/:id/:friendId" , verifyToken , addRemoveFriends);
+router.patch("/:id/social", verifyToken, updateSocialUrls);  // NEW: Must come before /:id/:friendId
+router.patch("/:id/:friendId", verifyToken, addRemoveFriends);
 
+// SEARCH //
 router.post('/search', (req, res) => {
     console.log('Received POST request to /users/search');
-    SearchUser(req , res);
+    SearchUser(req, res);
 });
+
 export default router;
