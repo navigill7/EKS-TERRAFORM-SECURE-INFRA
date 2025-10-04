3b0f83ca9494ba9974524cb7f5229e1c52ec90c0
Navinder Gill <nnavigill784@gmail.com>
2025-10-04 15:05:43 +0530

    adding app.js with some chnages

--- Development/client/src/App.js
+++ Development/client/src/App.js
@@ -1,31 +1,33 @@
-import { BrowserRouter ,Navigate , Routes , Route } from "react-router-dom";
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
 import HomePage from "scenes/homePage";
 import LoginPage from "scenes/loginPage";
 import AlumniPage from "scenes/alumniPage";
-import Navbar from "scenes/navbar";
 import ProfilePage from "scenes/profilePage";
-import { useMemo } from "react";
-import {  useSelector } from "react-redux";
-import { CssBaseline , ThemeProvider } from "@mui/material";
-import { createTheme } from "@mui/material/styles";
-import { themeSettings } from "theme";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
 
 function App() {
-  const mode = useSelector((state)=> state.mode);
-  const theme = useMemo(()=> createTheme(themeSettings(mode)) , [mode])
+  const mode = useSelector((state) => state.mode);
   const isAuth = Boolean(useSelector((state) => state.token));
+
+  useEffect(() => {
+    // Apply dark mode class to html element
+    if (mode === 'dark') {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+  }, [mode]);
+
   return (
-    <div className="App">
+    <div className={`App min-h-screen ${mode === 'dark' ? 'bg-grey-900 text-grey-100' : 'bg-grey-10 text-grey-700'} transition-colors duration-300`}>
       <BrowserRouter>
-       <ThemeProvider theme={theme}>
-         <CssBaseline/>
-          <Routes>
-            <Route path="/" element={<LoginPage/>} />
-            <Route path="/home" element={ isAuth ? <HomePage/> : <Navigate to="/" />}/>
-            <Route path="/profile/:userId" element={ isAuth ? <ProfilePage/> : <Navigate to="/"/>}/>
-            <Route path="//home/alumniPage" element={isAuth ? <AlumniPage/> : <Navigate to="/"/>}/>
-          </Routes>
-       </ThemeProvider>      
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/home" element={isAuth ? <HomePage /> : <Navigate to="/" />} />
+          <Route path="/profile/:userId" element={isAuth ? <ProfilePage /> : <Navigate to="/" />} />
+          <Route path="/home/alumniPage" element={isAuth ? <AlumniPage /> : <Navigate to="/" />} />
+        </Routes>
       </BrowserRouter>
     </div>
   );
