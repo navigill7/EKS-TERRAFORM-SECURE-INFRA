96b986d59e0a95785627c19ba3631a7a8d3d87b7
Navinder Gill <nnavigill784@gmail.com>
2025-12-07 22:53:41 +0530

    adding notificationStatics

---
+++ Development/notification_s/src/main/java/com/unilink/notification_service/dto/NotificationStatistics.java
@@ -0,0 +1,18 @@
+package com.unilink.notification_service.dto;
+
+
+
+//import lombok.AllArgsConstructor;
+//import lombok.Builder;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//
+//@Data
+//@Builder
+//@NoArgsConstructor
+//@AllArgsConstructor
+public class NotificationStatistics {
+    private String type;
+    private Long count;
+    private Long unread;
+}
