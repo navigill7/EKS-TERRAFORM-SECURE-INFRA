ed972bfd1d7b8ea50287c41b0455eff4acec7aa8
Navinder Gill <nnavigill784@gmail.com>
2025-12-07 22:45:22 +0530

    adding the redission for Queueing the messages

---
+++ Development/notification_s/src/main/java/com/unilink/notification_service/config/RedissonConfig.java
@@ -0,0 +1,37 @@
+package com.unilink.notification_service.config;
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RedissonConfig {
+
+    @Value("${spring.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.redis.port}")
+    private int redisPort;
+
+    @Value("${spring.redis.password:}")
+    private String redisPassword;
+
+    @Bean
+    public RedissonClient redissonClient() {
+        Config config = new Config();
+        String address = "redis://" + redisHost + ":" + redisPort;
+
+        if (redisPassword != null && !redisPassword.isEmpty()) {
+            config.useSingleServer()
+                    .setAddress(address)
+                    .setPassword(redisPassword);
+        } else {
+            config.useSingleServer()
+                    .setAddress(address);
+        }
+
+        return Redisson.create(config);
+    }
+}
